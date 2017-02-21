cc1ed9112cc12e248b62f18cc7a1b9428b0ed22d
Rob Campbell <rob10campbell@gmail.com>
2017-02-21 14:01:55 +0900

    repairing syntax (work in progress)

--- approxpi/plotapproxpi.jl
+++ approxpi/plotapproxpi.jl
@@ -1,4 +1,4 @@
-"""
+#=
 plotapproxpi.jl
 
 Purpose:
@@ -6,7 +6,7 @@ Purpose:
     Plot the approximation as a function of n to see if it converges to the true value of π.
     Plot the variance in each approximation as a function of 1/√n to see if the variance also decreases as we approach π (from statistics, variance should follow 1/√n -- plot of variance vs. 1/√n will be a straight line)
 
-"""
+=#
 
 using Distributions
 
@@ -29,12 +29,14 @@ plot(x=ns,y=πs,
     )
 
 sqrts = [(1/√n) for n in 1:1000];
-πvar = [var([appropi(0,1,n) for n in 1:N]) for N in 1:1000];
+πvar = [var([approxpi(0,1,n) for n in 1:N]) for N in 1:1000];
 
-plot(x=sqrts, y=πvar,
+p = plot(x=sqrts, y=πvar,
     Geom.point,
-    Cartesian(xmin=0, xmax=0.1, ymin=0),
+    #Cartesian(xmin=0, xmax=0.1, ymin=0),
     Guide.xlabel("1/√n"),
     Guide.ylabel("Variance of approximation of π"),
     Guide.title("Variance vs. 1/√n")
     )
+
+save(PNG()) #something or other like this
