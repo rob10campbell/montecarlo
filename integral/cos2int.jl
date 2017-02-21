c6a4e6e2434ddf487a522f7f43345b7e39f3c2a5
Rob Campbell <rob10campbell@gmail.com>
2017-02-21 11:33:09 +0900

    correct cos2

--- integral/cos2int.jl
+++ integral/cos2int.jl
@@ -1,23 +1,23 @@
 """
 cos2int(a,b,n)
 
-Using Monte Carlo methods to apprximate the integral ∫[cos(x)]^2 dx over the interval (a,b) for a number of random points n
+Using Monte Carlo methods to apprximate the integral ∫[cos(x)cos(y)]dx over the interval (a,b) for a number of random points n
 
 Requires Distributions.jl
 """
 function cos2int(a,b,n)
     under = 0
-    xmax = b-a
-    fmax = cos(xmax)^2
+    xmax = b - a
     dist = Uniform(a,b)
     for i in 1:n
         x = rand(dist)
         y = rand(dist)
-
-        if fmax * y < cos(x)^2
+        
+        if cos(xmax*x)cos(xmax*y) < cos(x)cos(y)
             under += 1
         end
     end
-    p = (under / n) * xmax * fmax
+    p = (under / n)
     return p
 end
+
