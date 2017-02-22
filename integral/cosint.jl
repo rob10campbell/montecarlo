97a29ea86190fbbf6f9a980f76890865d69251fa
Rob Campbell <rob10campbell@gmail.com>
2017-02-22 10:16:49 +0900

    correct png plotting

--- integral/cosint.jl
+++ integral/cosint.jl
@@ -37,7 +37,7 @@ pcos = plot(x=xs, y=approxcos,
     Guide.ylabel("Approximation of I"),
     Guide.title("Approximating I = [∫cos(x)dx] from 0 to 2π")
     );
-draw(PNG("ApproxIntCos.png"), 3inch, 3inch), pcos)
+draw(PNG("ApproxIntCos.png", 150mm, 150mm), pcos)
 
 sqrts = [(1/√x) for x in xs];
 vars = [var([cosint(0,2π,n) for n in 1:N]) for N in xs];
@@ -49,4 +49,4 @@ pcosvar = plot(x=sqrts, y=vars,
     Guide.ylabel("Variance of the approximation of I"),
     Guide.title("Variance of Approximations of I = [∫cos(x)dx] from 0 to 2π"),
     );
-draw(PNG("CosVar.png", 3inch, 3inch), pcosvar)
+draw(PNG("CosVar.png", 150mm, 150mm), pcosvar)
