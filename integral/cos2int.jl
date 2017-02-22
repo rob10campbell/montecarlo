266df1ba2928f2cfa9185dd208d0c85742d98ff0
Rob Campbell <rob10campbell@gmail.com>
2017-02-22 10:36:53 +0900

    plot approx of cos*cos

--- integral/cos2int.jl
+++ integral/cos2int.jl
@@ -1,23 +1,37 @@
 #=
-cos2int(a,b,n)
+cos2int.jl
 
-Using Monte Carlo methods to apprximate the integral ∫[cos(x)cos(y)]dx over the interval (a,b) for a number of random points n
-
-Requires Distributions.jl
+Purpose: Use Monte Carlo methods to apprximate the integral ∫[cos(x)cos(y)]dx over the interval (a,b) for a number of random points n
 =#
+using Distributions
+using Gadfly
+using Cairo
+using Fontconfig
+
 function cos2int(a,b,n)
     under = 0
     xmax = b - a
+    fmax = cos(xmax)cos(xmax)
     dist = Uniform(a,b)
     for i in 1:n
         x = rand(dist)
         y = rand(dist)
         
-        if cos(xmax*x)cos(xmax*y) < cos(x)cos(y)
+        if (fmax * y) < cos(x)cos(y)
             under += 1
         end
     end
-    p = (under / n)
+    p = (under / n) * xmax * fmax
     return p
 end
 
+xs = [x for x in 1:1000];
+cos2ys = [cos2int(0,2π,n) for n in xs];
+
+pcos2 = plot(x=xs, y=cos2ys,
+            Geom.point,
+            Guide.xlabel("Number of points (n)"),
+            Guide.ylabel("Approximation of I"),
+            Guide.title("Approximating I = ∫cosxcosy from 0 to 2π ")
+        );
+draw(PNG("Cos2Approx.png", 150mm, 150mm), pcos2)
