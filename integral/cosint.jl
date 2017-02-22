b96b039512615dde7e4d6d8a4e47256167714573
Rob Campbell <rob10campbell@gmail.com>
2017-02-22 09:54:10 +0900

    plot to png files

--- integral/cosint.jl
+++ integral/cosint.jl
@@ -1,13 +1,15 @@
-"""
+#=
 cosint.jl
 
 Purpose: 
 Use Monte Carlo methods to integrate y=cos(x) over an interval (a,b)
 Graph the approximation of [∫cos(x)dx] over (a,b)
 Graph the variance of the approximation with respect to 1/√n
-"""
+=#
 using Distributions
 using Gadfly
+using Cairo
+using Fontconfig
 
 function cosint(a,b,n)
     under = 0
@@ -29,20 +31,22 @@ end
 xs = [x for x in 1:1000];
 approxcos = [cosint(0,2π,n) for n in xs];
 
-plot(x=xs, y=approxcos,
+pcos = plot(x=xs, y=approxcos,
     Geom.point,
     Guide.xlabel("Number of points (n)"),
     Guide.ylabel("Approximation of I"),
     Guide.title("Approximating I = [∫cos(x)dx] from 0 to 2π")
-)
+    );
+draw(PNG("ApproxIntCos.png"), 3inch, 3inch), pcos)
 
 sqrts = [(1/√x) for x in xs];
 vars = [var([cosint(0,2π,n) for n in 1:N]) for N in xs];
 
-plot(x=sqrts, y=vars,
+pcosvar = plot(x=sqrts, y=vars,
     Coord.Cartesian(xmin=0, xmax=0.5, ymin=0, ymax=0.5),
     Geom.point,
     Guide.xlabel("1/√n"),
     Guide.ylabel("Variance of the approximation of I"),
     Guide.title("Variance of Approximations of I = [∫cos(x)dx] from 0 to 2π"),
-)
+    );
+draw(PNG("CosVar.png", 3inch, 3inch), pcosvar)
