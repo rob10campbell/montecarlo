7ee4d78bb07bbb04e94b758dee8620b4c7629d9e
Rob Campbell <rob10campbell@gmail.com>
2017-04-23 16:48:58 +0900

    reorganize test files

---
+++ approxpi/trials/plotapproxpi.jl
@@ -0,0 +1,43 @@
+#=
+plotapproxpi.jl
+
+Purpose:
+    Using Monte Carlo methods, approximate π for a variety of populations of random numbers (n).
+    Plot the approximation as a function of n to see if it converges to the true value of π.
+    Plot the variance in each approximation as a function of 1/√n to see if the variance also decreases as we approach π (from statistics, variance should follow 1/√n -- plot of variance vs. 1/√n will be a straight line)
+
+=#
+
+using Distributions
+using Gadfly
+using Cairo
+using Fontconfig
+
+include("approxpi.jl")
+
+ns1 = [n for n in 0:10:1000];
+ns2 = [n for n in 1000:100:100000];
+
+ns = append!(ns1,ns2);
+
+πs = [approxpi(0,1,n) for n in ns];
+
+pπ = plot(x=ns,y=πs,
+    Geom.point, yintercept=[π], Geom.hline,
+    Guide.xlabel("Number of random points (n)"),
+    Guide.ylabel("Value of π"),
+    Guide.title("Approximating π")
+    );
+draw(PNG("ApproxPi.png", 150mm, 150mm), pπ)
+
+sqrts = [(1/√n) for n in 1:1000];
+πvar = [var([approxpi(0,1,n) for n in 1:N]) for N in 1:1000];
+
+pvar = plot(x=sqrts, y=πvar,
+    Geom.point,
+    Coord.Cartesian(xmin=0, xmax=0.1, ymin=0, ymax=0.3),
+    Guide.xlabel("1/√n"),
+    Guide.ylabel("Variance of approximation of π"),
+    Guide.title("Variance vs. 1/√n")
+    );
+draw(PNG("VarApprox.png", 150mm, 150mm), pvar)
---
+++ integral/trials/cosint/cosint.jl
@@ -0,0 +1,54 @@
+#=
+cosint.jl
+
+Purpose: 
+Use Monte Carlo methods to integrate y=cos(x) over an interval (a,b)
+Graph the approximation of [∫cos(x)dx] over (a,b)
+Graph the variance of the approximation with respect to 1/√n
+=#
+using Distributions
+using Gadfly
+using Cairo
+using Fontconfig
+
+function cosint(a,b,n)
+    under = 0
+    xmax = b - a
+    fmax = 2 #know that max of cos(x) is 1 and the min of cos(x) is -1
+    #max - min = interval ; 1 - (-1) = 2
+    dist = Uniform(0,1)
+    for i in 1:n
+        x = rand(dist)
+        y = rand(dist)
+        
+        if fmax * y < cos(x)
+            under += 1
+        end
+    end
+    p = (under / n)
+    return p
+end
+
+xs = [x for x in 1:1000];
+approxcos = [cosint(0,2π,n) for n in xs];
+
+pcos = plot(x=xs, y=approxcos,
+    Geom.point,
+    Guide.xlabel("Number of points (n)"),
+    Guide.ylabel("Approximation of I"),
+    Guide.title("Approximating I = [∫cos(x)dx] from 0 to 2π")
+    );
+draw(PNG("CosIntApprox.png", 150mm, 150mm), pcos)
+
+#=
+sqrts = [(1/√x) for x in xs];
+vars = [var([cosint(0,2π,n) for n in 1:N]) for N in xs];
+
+pcosvar = plot(x=sqrts, y=vars,
+    Coord.Cartesian(xmin=0, xmax=0.5, ymin=0, ymax=0.5),
+    Geom.point,
+    Guide.xlabel("1/√n"),
+    Guide.ylabel("Variance of the approximation of I"),
+    Guide.title("Variance of Approximations of I = [∫cos(x)dx] from 0 to 2π"),
+    );
+draw(PNG("CosVar.png", 150mm, 150mm), pcosvar)=#
